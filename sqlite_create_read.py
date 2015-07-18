a4dac5f3ece2e8f82e82be5f7063e2de08548a51
geekgeek <solq87@gmail.com>
2015-07-18 19:29:16 +0200

    Create sqlite_create_read.py

---
+++ sqlite_create_read.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#http://zetcode.com/db/sqlitepythontutorial/
+#Create table and 
+import sqlite3 as lite
+import sys
+
+con = lite.connect('test.txt')
+
+def createTable():
+    with con:
+        cur = con.cursor()    
+        cur.execute("CREATE TABLE Cars(Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Price INT)")
+        cur.execute("INSERT INTO Cars(name,price) VALUES('Audi',52642)")
+        cur.execute("INSERT INTO Cars(name,price) VALUES('Toyota',52632)")
+        cur.execute("INSERT INTO Cars(name,price) VALUES('Volvo',58642)")
+ 
+def getTableData():
+    with con:    
+        cur = con.cursor()    
+        cur.execute("SELECT * FROM Cars")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print row
+
+createTable()
+
+#getTableData()
